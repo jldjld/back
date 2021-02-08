3822751b93fe90615d774c8776c72dc97d62c039
jldjld <jldsimplon@gmail.com>
2021-02-08 14:48:26 +0100

    Adding test.spec.ts file.

--- test/test.spec.ts
+++ test/test.spec.ts
@@ -1,3 +1,11 @@
 import * as chai from "chai";
 import * as assert from "assert";
 
+const expect = chai.expect
+
+describe("My first test", () => {
+
+    it("1 is equal to 1", () => {
+        expect(1).to.equal(1)
+    })
+})
