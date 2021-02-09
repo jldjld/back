94db5d542ce9a50a1b34c9537c9a3cceabeb6db4
Soph Brunier <soph.brunier@gmail.com>
2021-02-09 10:13:50 +0100

    add express module for API request

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,27 @@ import * as dotenv from "dotenv"
 dotenv.config();
 import { Config, DB } from "./service"
 
-console.log(Config.all())
+const express = require("express");
+const bodyParser = require("body-parser");
 
+const app = express();
+
+// parse requests of content-type: application/json
+app.use(bodyParser.json());
+
+// parse requests of content-type: application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// simple route
+app.get("/", (req, res) => {
+    res.json({ message: "Welcome to my Api." });
+});
+
+
+// set port, listen for requests
+app.listen(3000, () => {
+    console.log("Server is running on port 3000.");
+});
 
 // DB.query("SELECT * FROM users")
 //     .then(results => {
