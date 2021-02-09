d88ffcc20b8fee4599a76d180fb7d7c3785311ab
Soph Brunier <soph.brunier@gmail.com>
2021-02-09 15:35:16 +0100

    change host

--- pre-test.sh
+++ pre-test.sh
@@ -1,3 +1,3 @@
 
-cat ./test/create_db_test.sql | mysql -hlocalhost -utoto -p1234 
-cat ./test/mockup_db_test.sql | mysql -hlocalhost -utoto -p1234 simplon_groupx_test
+cat ./test/create_db_test.sql | mysql -h127.0.0.1 -utoto -p1234 
+cat ./test/mockup_db_test.sql | mysql -h127.0.0.1 -utoto -p1234 simplon_groupx_test
