ef2c06b2b0e259ed456c81dd6a3223cc0725f4e0
Soph Brunier <soph.brunier@gmail.com>
2021-02-09 10:03:49 +0100

    ajout des variables d'environnements

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,13 @@
-import { Args } from "./config/args"
-import { Config } from "./config/config.service"
-import { DB } from "./service/db.service"
+import * as dotenv from "dotenv"
+dotenv.config();
+import { Config, DB } from "./service"
 
-Args.required("env", ["local", "test", "beta", "prod"])
-const ENV = Args.get("env")
-//console.log(`Environment is ${ENV}`)
+console.log(Config.all())
 
-Config.init(ENV)
 
-DB.init({ 
-    host: Config.get("DB_HOST"),
-    user: Config.get("DB_USER"),
-    password: Config.get("DB_PASSWORD"),
-    dbname: Config.get("DB_NAME"),
-})
-
-DB.query("SELECT * FROM users")
-    .then(results => {
-        console.log(results)
-    }).catch(e => {
-        console.log(e)
-    })
+// DB.query("SELECT * FROM users")
+//     .then(results => {
+//         console.log(results)
+//     }).catch(e => {
+//         console.log(e)
+//     })
